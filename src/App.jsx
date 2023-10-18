b22c09627341e70271c962dc15a034f06893dfd8
Maksymilian Czadowski <maksymilian.czadowski@gmail.com>
2023-10-18 11:51:31 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        React + Vite app
+      </div>
+    </BrowserRouter>
+  );
+};
+export default App;
