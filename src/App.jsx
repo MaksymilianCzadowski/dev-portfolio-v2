566dd5403e58932df74b65bededb0e70369bc356
MCzadowski <czadomax@gmail.com>
2024-01-14 20:13:08 +0100

    base styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="App">
-        React + Vite app
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          {/* <Navbar /> */}
+          {/* <Hero /> */}
+        </div>
       </div>
     </BrowserRouter>
   );
